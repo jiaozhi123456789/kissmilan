3518ced2f92da73db7754be02928796c8d85db3e
jiaozhi123456789 <45158738+jiaozhi123456789@users.noreply.github.com>
2019-03-11 02:56:38 +0800

    Update Bottom.vue
    修改12

--- src/components/commons/Bottom.vue
+++ src/components/commons/Bottom.vue
@@ -5,7 +5,7 @@
 				 <router-link  tag='li'   to="/">
 				 	
 				 	  <i class="fa fa-home" aria-hidden="true"></i>
-                            <span>首页1</span>
+                            <span>首页12</span>
 				</router-link>
 		
 			
@@ -13,14 +13,14 @@
 				 <router-link  tag='li'  to="/goods/0">
 				<i class="fa fa-th-large" aria-hidden="true"></i>
 
-					<span>蛋糕名录1</span>
+					<span>蛋糕名录12</span>
 					</router-link>
 			
 		
 				 <router-link tag='li'   to="/user/order">
                        <i class="fa fa-file-text" aria-hidden="true"></i>
 
-				<span>我的订单1</span>
+				<span>我的订单12</span>
 				</router-link>
 		
 		
@@ -29,7 +29,7 @@
  <router-link tag='li'  to="/user">
  	<i class="fa fa-user" aria-hidden="true"></i>
 
-			<span>会员中心1</span>
+			<span>会员中心12</span>
 			</router-link>
 			
 		</ul>
