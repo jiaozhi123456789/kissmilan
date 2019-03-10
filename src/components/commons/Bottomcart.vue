14c0fc611b7f06007058233170912649e33a2d76
jiaozhi <1094602377@qq.com>
2019-03-11 02:16:32 +0800

    大修改jiaozhi1分支

--- src/components/commons/Bottomcart.vue
+++ src/components/commons/Bottomcart.vue
@@ -3,7 +3,7 @@
 	<div class="hanlde">
 		<i class="fa fa-check-square-o cur"></i>
 		商品总价：￥148.00
-		<span class="fr">去结算</span>
+		<span class="fr">去结算大修改1</span>
 	</div>
 	
 </template>
@@ -21,49 +21,37 @@
 <style lang="less" scoped>
  @import '../../styles/main.less';	
  
-.hanlde {
-  position: fixed;
-    right:50px;
-  background: red;
-}
 
-.hanlde>.fa.cur  {
-    color: #9a5b57;
-}
 
-.hanlde>.fa {
-    font-size: 22px;
-    vertical-align: middle;
-    margin-right: 20px;
-  
-}
-.fa {
-    display: inline-block;
-    font: normal normal normal 14px/1 FontAwesome;
- 
-    text-rendering: auto;
-    -webkit-font-smoothing: antialiased;
-
-}
-.fa-check-square-o:before {
-	.w(17.28);
-	.h(22);
-    content: "\F046";
+.div1{
+  background-color: orange;
 }
 .hanlde>span {
     padding: 0 20px;
     background: #492321;
-    color: #fff;
     text-indent: 0;
-    font-size: 14px;
+    font-size: 19px;
     letter-spacing: 2px;
 }
+ .fe{
+   display: inline-block;
+   font: normal normal normal 14px/1 FontAwesome;
+
+   text-rendering: auto;
+   -webkit-font-smoothing: antialiased;
+
+ }
 .fr {
 	.w(50);
 	.h(53);
     float: right;
     
 }
+ .fl {
+   .w(50);
+   .h(53);
+   float: left;
 
+ }
 
 </style>
